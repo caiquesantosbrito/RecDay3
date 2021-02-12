1c324c1f5ccd456c82e2f67fae5e7efe1fea28a9
FIC <FIC@info.local>
2021-02-12 16:55:15 -0300

    Controller e Models criados

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecDay3_InstaDev.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Interfaces/IUsuario.cs
@@ -0,0 +1,9 @@
+using RecDay3_InstaDev.Models;
+
+namespace RecDay3_InstaDev.Interfaces
+{
+    public interface IUsuario
+    {
+        void Cadastrar(Usuario user);
+    }
+}
