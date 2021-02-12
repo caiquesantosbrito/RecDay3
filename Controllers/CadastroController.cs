acb7780393bc3e516d300e4150ad2b2beae2eb83
FIC <FIC@info.local>
2021-02-12 17:12:34 -0300

    Cshtml adicionado

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace RecDay3_InstaDev.Controllers
 {
     public class CadastroController : Controller
-    {
+    {   
+        [Route("Cadastro")]
         public IActionResult Index()
         {
             return View();
